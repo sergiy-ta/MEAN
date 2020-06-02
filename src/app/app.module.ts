9129dbd0e182ac83a207663a30787885e14d7244
Serhii <serhiy819@gmail.com>
2020-06-02 20:54:10 +0300

    create form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
 
 import { AppComponent } from './app.component';
+import { ElementCreateComponent } from './element-create/element-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ElementCreateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+
+    BrowserAnimationsModule,
+
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/element-create/element-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-element-create',
+  templateUrl: './element-create.component.html',
+  styleUrls: ['./element-create.component.css']
+})
+export class ElementCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
